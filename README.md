9266bbfbf69831c9c96762822f50c61f8958857b
Takashi Imahiro <imahiro@erin.jp>
2024-02-04 15:47:50 +0900

    🎉 Release 1.0.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Copy Jira Issue Key to Clipboard
+
+Copy Jira Issue Key to Clipboard instead of Jira URL on Shift + Click.
+
+### 1.0.0
+
+- 🎉 Initial release
---
+++ js/content.js
@@ -0,0 +1,18 @@
+document.addEventListener('mousedown', (event) => {
+  if (event.shiftKey) {
+    const issueKey = findIssueKey(event.target);    
+    if (issueKey) {
+      Promise.resolve()
+      .then(() => new Promise((resolve, _reject) => {
+        setTimeout(() => resolve(), 100);
+      }))
+      .then(() => {
+        navigator.clipboard.writeText(issueKey);
+      })
+    }
+  }
+});
+
+const findIssueKey = (node) => {
+  return node?.closest('.issue_view_permalink_button_wrapper')?.previousElementSibling?.querySelector('li a span')?.innerHTML;
+}
