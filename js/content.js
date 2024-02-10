211b6e51a00cbb48d5eac4e7f382591b8d32f426
Takashi Imahiro <imahiro@erin.jp>
2024-02-11 08:54:27 +0900

    ♻️ use dummy node to prevent copy link event

--- js/content.js
+++ js/content.js
@@ -1,3 +1,29 @@
+let srcNode;
+const dummyNode = document
+  .createRange()
+  .createContextualFragment(
+    '<div id="BreadcrumbCurrentIssue"></div>'
+  ).firstChild;
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Shift") {
+    const node = document.querySelector("#BreadcrumbCurrentIssue");
+    if (node) {
+      node.parentNode.replaceChild(dummyNode, node);
+      srcNode = node;
+    }
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "Shift") {
+    const node = document.querySelector("#BreadcrumbCurrentIssue");
+    if (node) {
+      node.parentNode.replaceChild(srcNode, node);
+    }
+  }
+});
+
 document.addEventListener("mousedown", (event) => {
   if (event.shiftKey && (event.altKey || event.metaKey)) {
     const issueKey = findIssueKey(event.target);
@@ -14,25 +40,16 @@ document.addEventListener("mousedown", (event) => {
 });
 
 const writeToClipboard = (text) => {
-  Promise.resolve()
-    .then(
-      () =>
-        new Promise((resolve, _reject) => {
-          setTimeout(() => resolve(), 100);
-        })
-    )
-    .then(() => {
-      navigator.clipboard.writeText(text);
-    });
+  navigator.clipboard.writeText(text);
 };
 
 const findIssueKey = (node) => {
-  return node?.closest(".issue_view_permalink_button_wrapper")
+  return node.closest(".issue_view_permalink_button_wrapper")
     ?.previousElementSibling?.textContent;
 };
 
 const findIssueSummary = (node) => {
   return node
-    ?.closest("#jira-issue-header")
+    .closest("#jira-issue-header")
     ?.nextElementSibling?.querySelector('[role="presentation"]')?.textContent;
 };
